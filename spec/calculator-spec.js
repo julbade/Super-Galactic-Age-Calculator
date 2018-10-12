4f8f6f59643674bf39cdffb18173de21d1c458a9
julius bade <julbade21@gmail.com>
2018-10-12 10:55:58 -0700

    added user interface and test methods

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -4,11 +4,28 @@ import { Calculator } from './../src/calculator.js';
 describe ('Calculator Test', function() {
   let calculator = new Calculator;
 
+
+  it('should calculate correct age for Earth years', function(){
+    expect(calculator.ageOnEarth(27)).toEqual(27)
+  });
+
   it('should calculate correct age for Mercury years', function(){
     expect(calculator.ageOnMercury(27)).toEqual(112)
   });
 
-  it('should calculate correct age for Eaerth years', function(){
-    expect(calculator.ageOnEarth(27)).toEqual(27)
+  it('should calculate correct age for Venus years', function(){
+    expect(calculator.ageOnVenus(27)).toEqual(43)
   });
+
+  it('should calculate correct age for Mars years', function(){
+    expect(calculator.ageOnMars(27)).toEqual(14)
+  });
+  
+  it('should calculate correct age for Jupiter years', function(){
+    expect(calculator.ageOnJupiter(27)).toEqual(2)
+  });
+
+  // it('should calculate correct age for Earth years', function(){
+  //   expect(calculator.ageOnEarth(27)).toEqual(27)
+  // });
 });
